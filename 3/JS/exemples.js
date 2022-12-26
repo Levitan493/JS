60021e8b1057eeea5a3c27d84303d155c4b12512
Levitan493 <113470635+Levitan493@users.noreply.github.com>
2022-12-26 22:35:21 +0200

    Add files via upload

--- 3/JS/exemples.js
+++ 3/JS/exemples.js
@@ -1,8 +1,19 @@
-function printArg(value = 'Empty', printTime){
-    if(printTime === undefined){
-        printTime = 3;
-    }
-    for(let i = 0; i<printTime; i++){
-        console.log(value);
-    }
-}
+// function printArg(value = 'Empty', printTime){
+//     if(printTime === undefined){
+//         printTime = 3;
+//     }
+//     for(let i = 0; i<printTime; i++){
+//         console.log(value);
+//     }
+// }
+
+// let hour =3660;
+// let day = hour*24;
+// let mount = day*31;
+// let year = day*365;
+// let yearAge = prompt('Скільки років?');
+// let mountAge = prompt('Cкільки місяців');
+// let userAge = 
+// ююююююююююююююююююююююююююююююююююю
+// Не закінчено!
+
